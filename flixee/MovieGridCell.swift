af8b4e8326471cb52bc937e522731dcd1471d4ce
OldWorldForest <oldworldforest@gmail.com>
2020-09-30 23:06:52 -0700

    Part 2 of the assignment

---
+++ flixee/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixee
+//
+//  Created by Aldo Flores on 9/30/20.
+//  Copyright © 2020 Aldo Flores. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
